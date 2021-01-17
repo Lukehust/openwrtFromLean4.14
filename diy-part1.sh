89083544fde47b44a3c3c88f36f6506fdf1fcb9f
Lukehust <lukehust@foxmail.com>
2021-01-17 12:13:15 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/BoringCat/luci-app-mentohust package/myapp/luci-app-mentohust
+
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk package/myapp/MentoHUST-OpenWrt-ipk
